789fbfead2b2e63747c0c36dd7442602ad15a412
pigchick3n <league@WorthyMini-5.attlocal.net>
2018-10-26 17:00:48 -0700

    guestbook

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -2,6 +2,7 @@ package intro_to_array_lists;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,6 +14,8 @@ public class GuestBook implements ActionListener {
 	JPanel panel = new JPanel();
 	JButton addname = new JButton("Add Name");
 	JButton viewname = new JButton("View Name");
+	int counter;
+	ArrayList<String> names = new ArrayList<String>();
 	public static void main(String[] args) {
 		GuestBook gb = new GuestBook();
 		gb.run();
@@ -22,6 +25,9 @@ public class GuestBook implements ActionListener {
 		panel.add(addname);
 		panel.add(viewname);
 		addname.addActionListener(this);
+		viewname.addActionListener(this);
+		frame.setVisible(true);
+		frame.pack();
 	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -33,7 +39,19 @@ public class GuestBook implements ActionListener {
 	// Guest #4: Donny Doners
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton) e.getSource();
 		// TODO Auto-generated method stub
-		JOptionPane.showInputDialog("Enter a name");
+		if(buttonPressed==addname) {
+			counter++;
+		String x = JOptionPane.showInputDialog("Enter a name");
+		names.add(x);
+		}else if (buttonPressed==viewname) {
+			String s="";
+			for(int i = 0; i < names.size(); i++){
+				s += "Guest #"+ i+": "+names.get(i)+"\n";
+				
+			}
+			JOptionPane.showMessageDialog(null, s);
+			}
 	}
-}
+	}
